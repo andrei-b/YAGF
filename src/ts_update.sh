1bf3a7b8dc84e9d6f3954a2d5a8492158b0a0ac0
andrei.borovsky@gmail.com <andrei.borovsky@gmail.com@bb55e26e-657b-11de-982f-55b50260c994>
2011-10-22 06:04:19 +0000

    git-svn-id: http://yagf.googlecode.com/svn/trunk@187 bb55e26e-657b-11de-982f-55b50260c994

--- src/ts_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-lupdate -noobsolete mainform.cpp mainform.ui popplerdialog.ui configdialog.ui main.cpp moc_mainform.cxx spellchecker.cpp FileToolBar.cpp moc_FileToolBar.cxx -ts yagf_ru.ts yagf_lt.ts yagf_de.ts yagf_pl.ts yagf_uk.ts
